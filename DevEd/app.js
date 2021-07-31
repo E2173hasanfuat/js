25d537055c72034a0f756500c93f0d3563a1b931
E2173hasanfuat <69896039+E2173hasanfuat@users.noreply.github.com>
2021-07-31 23:56:08 +0200

    new

--- DevEd/app.js
+++ DevEd/app.js
@@ -1,1 +1,35 @@
-console.log(hi);
+// var life = 100;
+
+// life = life - 10;
+
+// var name = 'hasan';
+
+// var checkout = false;
+
+// var box = null;
+
+// console.log(box);
+
+// let life = 100;
+
+// life = life - 50;
+
+// console.log(life);
+
+// const you = 'hasan';
+
+// function toUpper(text) {
+//   const upperCased = text.toUpperCase();
+//   console.log(upperCased);
+// }
+
+// const toUpper = () => {};
+
+// console.log('Hello my name is ' + 'hasan');
+
+// const name = 'hasan';
+// const age = 33;
+
+// const combined = name + age;
+// console.log(combined);
+// console.log(typeof combined);
